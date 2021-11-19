342406894ced2cfbd02be724bd10343c1734c23c
Alexandre Dulaunoy <a@foo.be>
2021-11-19 11:21:29 +0100

    new: [changelog] build script added

---
+++ build-changelog.sh
@@ -0,0 +1,12 @@
+cd ../misp-taxonomies
+gitchangelog >../misp-website/Changelog-misp-taxonomies.txt
+cd ../misp-warninglists
+gitchangelog >../misp-website/Changelog-misp-warninglists.txt
+cd ../misp-objects
+gitchangelog >../misp-website/Changelog-misp-objects.txt
+cd ../misp-galaxy
+gitchangelog >../misp-website/Changelog-misp-galaxy.txt
+cd ../misp-modules
+gitchangelog >../misp-website/Changelog-misp-modules.txt
+cd ../PyMISP
+gitchangelog >../misp-website/Changelog-PyMISP.txt
